76e4fef27f3168635c7889349bc8812e5dd0d236
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-10-22 11:06:11 -0400

    Card front image added

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ window.title("Flash")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0)
 
 
 
