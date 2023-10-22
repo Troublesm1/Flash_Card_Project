a89f384a2ceada1ea3e57f2d9763d0a9f39e7641
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-10-22 12:40:35 -0400

    next card function started

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+print(data)
+
+def next_card():
+    pass
+
 
 window = Tk()
 window.title("Flash")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+# --------------- UI -----------------------
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
@@ -16,11 +25,11 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 # --------------- Buttons -----------------------
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
 
